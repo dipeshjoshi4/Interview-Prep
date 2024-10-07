1c97e5c0f853b3f5ae51eb70b511eedf9c2989ce
Your Name <dipeshj2310@gmail.com>
2024-10-07 16:03:02 +0530

    half prototype

---
+++ JS Concept/13 Prototypes/script.js
@@ -0,0 +1,107 @@
+/*
+
+Ques 1: What will be the output of the following code ?
+Explanation : The output will be:
+Driving a vehicle
+Driving a car
+
+This is because vehicle.drive() invokes the drive() method from the Vehicle prototype, while car.drive() invokes the overridden drive() method in the Car prototype.
+
+Ques 2: Explain the difference between __proto__ and prototype in JavaScript.
+Explanation:__proto__: It points to the prototype of an object and is used for inheritance and accessing the prototype chain.
+prototype: It exists on constructor functions to set up inheritance for objects created by that function, defining shared properties and methods for instances.
+
+Ques 3: What is setPrototypeOf ?
+Explanation : setPrototypeOf is a method used to set the prototype(the object's internal [[Prototype]]) of a specified object to another object or null. It allows changing the prototype dynamically after an object has been created.
+
+Ques 4: What is instanceof?
+Explanation : instanceof is an operator that checks if an object is an instance of a specific constructor or its prototype chain.It returns true if the object is an instance of the constructor or a constructor's prototype chain.
+
+Ques 5: How can you create an object without a prototype in JavaScript ?
+ Explanation : You can create an object without a prototype by using Object.create(null).This creates an object with no prototype chain, making it free from any inherited properties or methods.
+
+Ques 6: What will be the output of the following code ?
+Explanation : The output will be:
+10
+20
+30
+
+This is because obj1 is an instance of A with foo as 10, obj2 is an instance of B with foo as 20 (overridden from A), and obj3 is an instance of C with foo as 30 (overridden from both A and B).
+
+Ques 7: Deep Clone an object in JS
+Explanation: The deepClone function recursively creates a deep copy of an object, including nested objects and arrays, ensuring that the cloned object is completely independent of the original object.
+
+*/
+
+//?Object
+// let obj = {
+//     name: "dipesh",
+//     age: 25,
+// }
+// - Object prototypes which is given by js in output of every object.Through which we can maniupulate the object and access different properties
+//- we can call in console like ---> Object.prototype or objname.__proto__
+
+//?Variable is object
+// let num = 10;
+// let name = "coder";
+// let bool = true;
+//all of this diferent kind of datatypes or you could say variable are have object prototype .means its object
+// num.__proto__
+// name.__proto__.__proto__
+// num.__proto__.__proto__
+
+//?function is object
+// function add(a, b) {
+//     return a + b;
+// }
+// console.log(add(1, 2));
+//- so function also have prototype of object and with this we cann add key in function and it also end of the day became object
+// console.log(add().__proto__) //function prototype
+// console.log(add().__proto__.__proto__) //object prototype
+
+//?Prototype Chaining
+
+// let person = {
+//     name: "dipesh",
+//     age:25,
+// }
+// console.log(person.alice()); //?ERROR: person.alice() is not function
+
+// let person2 = {
+//     name: "dipesh",
+//     age: 25,
+//     toString: () => {
+//         console.log("convert to string")
+//     }
+// }
+// console.log(person2.toString()); //?convert to string
+
+// let person2 = {
+//     name: "dipesh",
+//     age: 25,
+// }
+// console.log(person2.toString()); //?[object object]
+
+//- this is all about prototype chaining
+//- here toString function find in person object . and then if not there then find in object prototype
+
+//?Example
+
+// let person = {
+//     name: "dipesh",
+//     age: 25,
+// }
+// let additional = {
+//     name: "dipesh joshi",
+//     username: "Coder",
+//     alias: function () {
+//         console.log(`${this.name} is also know as ${this.username}`);
+//     }
+// }
+// person.__proto__ = additional;
+// console.log(person.alias()) //? dipesh is also know as Coder
+
+// so here person object first try and search every property inside his object  and if the property or function not person object then he goes to  object prototype .and we here inherit additional object prototype
+// so we get some property and execute it
+
+//?Concept of constructor and new keyword
