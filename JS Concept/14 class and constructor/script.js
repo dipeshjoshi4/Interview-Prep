2640f8d901dbb7c8fa5aa9144aaa05e9ae56f231
Your Name <dipeshj2310@gmail.com>
2024-10-15 22:57:30 +0530

    14 and 15

--- JS Concept/14 class and constructor/script.js
+++ JS Concept/14 class and constructor/script.js
@@ -45,10 +45,10 @@ They allow for more controlled modification and retrieval of property values, su
 
 */
 
-//?Class Defination
+//! Class Defination
 //class is the blueprint of object.they have method and property.which define their behaviour and charcterstics of that objects
 
-//?classes Usecase
+//! classes Usecase
 //- class come to us a complete bundel from which
 //- we can create object,
 //- we can create constructor inside of objects
@@ -57,10 +57,18 @@ They allow for more controlled modification and retrieval of property values, su
 
 //!Example:1
 
-//now this class is syntax like object have method meand function intro and properties come from constructor
-//class teacher(x,y) dont have any input values in there().
-// so we use constructor so if you want then take input from user and bind with your class.
-//- whenever new object created (here coder) .this constructor job is provide this property
+//- now this class is syntax like object have method mean function intro and properties come from constructor
+//- class teacher(x,y) dont have any input values(take argument place) in there().
+//- so we use constructor so if you want then take input from user and bind with your classlike this
+//?CODE
+// constructor(name, channel) {
+//         this.name = name;
+//         this.channel = channel;
+//     }
+
+//- in class we have no need to write "function intro(){return "XYXZ"}"" => instead we have simply write "intro(){return "XYXZ"}"
+//- whenever new object created (here coder).this constructor job is provide this property
+
 // class Teacher {
 //     constructor(name, channel) {
 //         this.name = name;
@@ -71,6 +79,7 @@ They allow for more controlled modification and retrieval of property values, su
 //     }
 // }
 // const coder = new Teacher("Dipesh", "Learn Code")
+// console.log(coder.intro())
 
 
 //!Example-2
@@ -85,7 +94,7 @@ They allow for more controlled modification and retrieval of property values, su
 //         return `Hey its ${this.name}! and welcome to ${this.channel}`
 //     }
 //     like() {
-//         this.videoLikes++//?Access in class | you  can access into function like this => this.intro()
+//         this.videoLikes++//?Access in class | you  can access intro function like this => this.intro()
 //         return `liked this video! Current Likes : ${this.videoLikes}`;
 //     }
 // }
@@ -93,7 +102,7 @@ They allow for more controlled modification and retrieval of property values, su
 // const coder2 = new Teacher("mahesh", "learn offline")
 
 //? so here due to like count increment when the function called.it persist the old count so everytime call we gain incremental value
-//? now if we want can make 2 sperate object from one class with our dependent on each other.thats power of js class
+//? now if we want can make 2 sperate object from one class without dependent on each other.thats power of js class
 // console.log(coder2.like()) //? liked this video! Current Likes: 1
 // console.log(coder);        //? Teacher { name: 'Dipesh', channel: 'Learn Code', videoLikes: 0 }
 // console.log(coder2);       //? Teacher { name: 'mahesh', channel: 'learn offline', videoLikes: 1 }
@@ -101,12 +110,13 @@ They allow for more controlled modification and retrieval of property values, su
 //!Example-3
 //!Converting to Function Constructors
 
-//?s-1- we need to create constructor because function dont have such freedom to take input as constructor
+// //?s-1- we need to create constructor because function dont have such freedom to take input as constructor
 // function Teacher(name, channel, likes = 0) {
 //     this.name = name
 //     this.channel = channel
 //     this.videoLikes = likes
 // }
+
 // //?s-2- class have luxry to define function inside but we dont have so we have to prototype
 // Teacher.prototype.intro = function () {
 //     return `Hey its ${this.name}! and welcome to ${this.channel}`
@@ -115,6 +125,7 @@ They allow for more controlled modification and retrieval of property values, su
 //     this.videoLikes++
 //     return `liked this video! Current Likes : ${this.videoLikes}`;
 // }
+
 // const coder = new Teacher("Dipesh", "Learn Code")
 // const coder2 = new Teacher("mahesh", "learn offline")
 
@@ -122,19 +133,19 @@ They allow for more controlled modification and retrieval of property values, su
 
 //!Example
 
-class Teacher {
-    constructor(name, channel, likes = 0) {
-        this.name = name;
-        this.channel = channel;
-        this.videoLikes = likes;
-    }
-    intro() {
-        return `Hey its ${this.name}! and welcome to ${this.channel}`
-    }
-    like() {
-        this.videoLikes++//?Access in class | you  can access into function like this => this.intro()
-        return `liked this video! Current Likes : ${this.videoLikes}`;
-    }
-}
-const coder = new Teacher("Dipesh", "Learn Code")
-const coder2 = new Teacher("mahesh", "learn offline")
+// class YoutubeTeacher extends Teacher{
+//     constructor(name, channel, likes = 0) {
+//         this.name = name;
+//         this.channel = channel;
+//         this.videoLikes = likes;
+//     }
+//     intro() {
+//         return `Hey its ${this.name}! and welcome to ${this.channel}`
+//     }
+//     like() {
+//         this.videoLikes++//?Access in class | you  can access into function like this => this.intro()
+//         return `liked this video! Current Likes : ${this.videoLikes}`;
+//     }
+// }
+// const coder = new YoutubeTeacher("Dipesh", "Learn Code")
+// const coder2 = new YoutubeTeacher("mahesh", "learn offline")
