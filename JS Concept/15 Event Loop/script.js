6aed826ac8342fe44ceb2e94ed118aa99bb8b701
Your Name <dipeshj2310@gmail.com>
2024-10-18 22:06:05 +0530

    add event loop question

--- JS Concept/15 Event Loop/script.js
+++ JS Concept/15 Event Loop/script.js
@@ -2,8 +2,7 @@
 
 /*
 
-Ques 1 - What is Event Loop?
- The Event Loop in JavaScript is a mechanism responsible for managing asynchronous behavior in a single-threaded environment. It acts like a traffic controller, ensuring tasks are executed in an orderly manner by processing pending tasks in queues (microtasks and macrotasks).
+
 
 Ques 2 - Why do we need the event loop to manage these task queue and microtask queue?
 Explanation: The event loop is necessary to handle asynchronous operations in JavaScript effectively. It manages task queues and microtask queues to ensure that tasks are executed efficiently without blocking the main thread.
@@ -73,4 +72,52 @@ This happens because the pause function returns a promise that resolves after 10
 // },0)
 // console.log("3");
 //? this code is executed Asynchronous way thats why printed 1 3 2 line by
-//?what is the reason behind code 2 work Asynchronous way. thats why we have understand Event loop 
+//?what is the reason behind code 2 work Asynchronous way. thats why we have understand Event loop
+
+//!Event loop
+
+// The Event Loop in JavaScript is a mechanism responsible for managing asynchronous behavior in a single - threaded environment.It acts like a traffic controller, ensuring tasks are executed in an orderly manner by processing pending tasks in queues(microtasks and macrotasks).
+
+//- for understanding it goes to this website  => https://www.jsv9000.app/
+
+//? take this Synchronous code as Example see how it works
+
+// function fifth() {
+//     console.log("reach the end")
+// }
+// function fourth() { fifth() }
+// function third() { fourth() }
+// function second() { third() }
+// function first() { second() }
+// first()
+
+//? Now Asynchronous Code
+
+// console.log("1");
+// setTimeout(() => {
+//     console.log("2");
+// },0)
+// console.log("3");
+
+//?Undestanding of this Asynch code
+
+// 1. first clg(1) is executed
+// 2. async task goes into Task Queue untill call stackn is empty
+// 3. second clg(2) is executed
+// 4. now clg(1) and clg(2 both executed so the call stack empty
+// 5. async code comes in call stack then its executed .
+// 6. after all this call stack is again empty
+
+//?Notes -
+// js single thread language .
+// its can not wait to for any operation to complete.so its first move on to the synchronours part then its move on to async part
+// in call stack = first in last out
+// in Task Queue = First in first OUt
+
+//? What is Task Queue
+//-  while executed synchronous code all the asynchoronus code will be go in task queue
+// - async code means what is not part of js but its all the browser web api and Dom events. all the window object
+
+//! js single thread language but how so you to explain all the pointes above mention
+
+
