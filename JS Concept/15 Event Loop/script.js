1cccc827d459f39805b6f095eb842072641bdb38
Your Name <dipeshj2310@gmail.com>
2024-10-20 10:40:32 +0530

    add

--- JS Concept/15 Event Loop/script.js
+++ JS Concept/15 Event Loop/script.js
@@ -1,62 +1,5 @@
 
 
-/*
-
-
-
-Ques 2 - Why do we need the event loop to manage these task queue and microtask queue?
-Explanation: The event loop is necessary to handle asynchronous operations in JavaScript effectively. It manages task queues and microtask queues to ensure that tasks are executed efficiently without blocking the main thread.
-
-Ques 3 - What is the output?
-Explanation: The output will be:
-
-Start
-running..
-End
-
-This is because the blockMainThread() function blocks the main thread for approximately 3 seconds before logging "running.." and then "End" sequentially.
-
-Ques 4 - What is the output?
-Explanation: The output will be:
-
-d Runs
-c
-b
-a
-
-This is because the function d() is executed synchronously, while the setTimeout functions are added to the task queue and executed in the order of their specified timeouts.
-
-Ques 5 - What's the output?
-Explanation: The output will be:
-
-2
-2
-2
-
-This happens because in both versions of the a() function, the setTimeout functions capture the final value of i (which is 3 after the loop completes) due to closure, resulting in all three console.log(i) statements logging 2 after waiting for their respective timeouts.
-
-Ques 6 - What's the Output?
-Explanation: The output will be:
-
-e Runs
-b Runs
-c Runs
-d Runs
-
-This occurs because promises and their respective then handlers are executed as microtasks, following the microtask queue's order, and then the setTimeout callback runs as a macrotask after the microtasks are completed.
-
-Ques 7 - What's the Output?
-Explanation: The output will be:
-
-Start
-resolved: 1.3
-
-This happens because the pause function returns a promise that resolves after 1000 milliseconds, and the then handler calculates the time difference between the start and end times, logging "resolved: 1.3" after approximately 1.3 seconds.
-
-
-*/
-
-
 //!Before understanding eventloop lets understand what are things are affected by
 
 //?CODE:1
@@ -183,5 +126,151 @@ This happens because the pause function returns a promise that resolves after 10
 
 //!-Que-1-Event loop
 
-// The Event Loop in JavaScript is a mechanism responsible for managing asynchronous behavior in a single - threaded environment.
+//The Event Loop in JavaScript is a mechanism responsible for managing asynchronous behavior in a single - threaded environment.
 //It acts like a traffic controller, ensuring tasks are executed in an orderly manner by processing pending tasks in queues(microtasks and macrotasks).
+
+//!Ques 2 - Why do we need the event loop to manage these task queue and microtask queue ?
+//The event loop is necessary to handle asynchronous operations in JavaScript effectively.
+//It manages task queues and microtask queues to ensure that tasks are executed efficiently without blocking the main thread.
+
+//?Example
+// console.log("start");
+// setTimeout(() => {
+//     console.log("Inside SetTimeout(TaskQueue)")
+// }, 0)
+// Promise.resolve().then(() => {
+//     console.log("Inside Promise (microTask)");
+// })
+// console.log("End")
+
+//?Note : here we look at setTimeout is 0 time that doesnt mean it execute first because priority queue which micro task queue dose not have that function.its all because of event loop
+
+
+//!Ques 3 - What is the output?
+// This is because the blockMainThread() function blocks the main thread for approximately 3 seconds before logging "running.."
+// and then "End" sequentially.
+// when we fetching Api genrally we dont this kind of code we use webapi browser given function so that our code will be handle by event loop and it cant block our main thread
+
+//?CODE
+// blockMainThread();
+// console.log("start");
+// function blockMainThread() {
+//     const start = Date.now();
+//     while(Date.now() - start < 3000) { }
+//     console.log("running");
+// }
+// console.log("End");
+
+//? output
+// Start
+// running..
+// End
+
+//!Ques 4 - What is the output?
+//This is because the function d() is executed synchronously, while the setTimeout functions are added to the task queue and executed in the order of their specified timeouts.
+
+//?CODE
+// setTimeout(function a() {console.log("a");}, 1000);
+// setTimeout(function b() {console.log("b");}, 500);
+// setTimeout(function c() {console.log("c");}, 0);
+// function d() {
+//     console.log("d runs");
+// }
+// d();
+
+//? output
+// d Runs
+// c
+// b
+// a
+
+
+//! Ques 5 - What's the output?
+// This happens because in both versions of the a() function, the setTimeout functions capture the final value of i(which is 3 after the loop completes) due to closure, resulting in all three console.log(i) statements logging 2 after waiting for their respective timeouts.
+
+//? CODE
+// function a() {
+//     for (var i = 0; i < 3; i++){
+//         setTimeout(function log() {
+//             console.log(i)
+//         },i*1000)
+//     }
+// }
+// a()
+
+//? output
+// 2
+// 2
+// 2
+///task queue - log()    log()    log()
+//               iref     i ref    i ref
+// if there is let insted of var . so let have block scope closure and the out will be 0 1 2
+
+
+
+//! Ques 6 - What's the Output?
+//This occurs because promises and their respective then handlers are executed as microtasks, following the microtask queue's order, and then the setTimeout callback runs as a macrotask after the microtasks are completed.
+
+//?CODE
+// Promise.resolve()
+//   .then(function a() {
+//     Promise.resolve().then(
+//       setTimeout(function d() {
+//         console.log("d Runs");
+//       }, 0)
+//     );
+//     Promise.resolve().then(function e() {
+//       console.log("e Runs");
+//     });
+//     throw new Error("Error Occured!");
+//     Promise.resolve().then(function f() {
+//       console.log("f Runs");
+//     });
+//   })
+//   .catch(function b() {
+//     console.log("b Runs");
+//   })
+//   .then(function c() {
+//     console.log("c Runs");
+//   });
+
+//? output
+// e Runs
+// b Runs
+// c Runs
+// d Runs
+//Task queue - d()
+//micro Task - e() b() c()
+//call stack - a() comes and execute
+
+//! Ques 7 - What's the Output?
+// This happens because the pause function returns a promise that resolves after 1000 milliseconds, and the then handler calculates the time difference between the start and end times, logging "resolved: 1.3" after approximately 1.3 seconds.
+
+//?CODE
+// function pause(millis) {
+// return new Promise(function p(resolve) {
+//     setTimeout(function s() {
+//         resolve("resolved");
+//     }, millis);
+// });
+// }
+
+// const start = Date.now();
+// console.log("Start");
+
+// pause(1000).then((res) => {
+//     const end = Date.now();
+//     const secs = (end - start) / 1000;
+//     console.log(res, ":", secs);
+// });
+
+//?Output
+// Start
+// resolved: 1.5
+
+// Call Stack      - pause(1000)  p(resolve)
+// MicroTask Queue -
+// Task Queue      - s()
+
+
+
