765bc6387fbd99ce1fc861733189f0b80324bfcf
Your Name <dipeshj2310@gmail.com>
2025-07-30 19:50:34 +0530

    loop in loop tricky question

--- NamsteDSA/WarmUp/5_Loop-in-Loop/tricky.js
+++ NamsteDSA/WarmUp/5_Loop-in-Loop/tricky.js
@@ -1,2 +1,43 @@
 
 //!--------------loop in loop tricky example-----------------------
+
+//?Example-1
+// for (let i = 0; i < 3; i++) {
+//     for (let j = 0; j < i; j++) {
+//         console.log(i, j)
+//     }
+// }
+//?OUTPUT
+// 1 0
+// 2 0
+// 2 1
+
+//?Example-2
+// for (let i = 0; i < 5; i++) {
+//     for (let j = 0; j <= i; j++) {
+//         console.log("i=" + i + "j=" + j)
+//     }
+// }
+//?OUTPUT
+/*
+00
+
+10
+11
+
+20
+21
+22
+
+30
+31
+32
+33
+
+40
+41
+42
+43
+44
+
+*/
