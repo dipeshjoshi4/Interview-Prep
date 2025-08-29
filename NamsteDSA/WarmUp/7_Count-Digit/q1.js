35358fccbd27e7ac11a7072d15a449731349fa43
dipeshjoshi4 <dipeshj2310@gmail.com>
2025-08-29 20:11:44 +0530

    count digit lect-7 finish

--- NamsteDSA/WarmUp/7_Count-Digit/q1.js
+++ NamsteDSA/WarmUp/7_Count-Digit/q1.js
@@ -15,15 +15,60 @@
 // Input: 0
 // Output: 1
 
-function result(n) {
 
-    let n = 259;
+//?Example-1
+// function result() {
+//     let n = 259;
+//     let count = 0;
+//     while (n > 0) {
+//         n = Math.floor(n / 10); //To Avoid The Decimal
+//         count++;
+//     }
+//     return count;
+// }
+// console.log(result());
+
+//?Example-2
+// function countDigits2(n) {
+//     let count = 0
+//     while (n > 0) {
+//         n = Math.floor(n / 10);
+//         count++
+//     }
+//     return count;
+// }
+// let num2 = 655445;
+// let result1 = countDigits2(num2);
+// console.log(result1); //6
+
+
+
+//?Example-3     |corner Case - 0 | if n is 0 then answer comes 0 but its Actual Answer Should be 0
+
+// function countDigits(n) {
+//     if (n == 0) return 1
+//     let count = 0;
+//     while (n > 0) {
+//         n = Math.floor(n / 10);
+//         count++
+//     }
+//     return count;
+// }
+// let num3 = 0
+// let result = countDigits(num3)
+// console.log(result); //1
+
+//?Example-4     |corner Case - Negative Number | if n is Negative(-2023) then answer comes 0 but its Actual Answer Should be 4
+
+function countDigits3(n) {
+    n = Math.abs(n)    // converting negtive number into positive
     let count = 0;
     while (n > 0) {
-        n = n / 10;
-        count++;
+        n = Math.floor(n / 10)
+        count++
     }
     return count;
-
 }
-result();
+let num4 = -2023
+let result = countDigits3(num4);
+console.log(result); //4
